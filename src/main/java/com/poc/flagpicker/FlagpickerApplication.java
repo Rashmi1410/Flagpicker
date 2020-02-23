9133738c7cc6436eddc74c8ff7f0e6fad41e7b20
Rashmi <rashmiagrawal3008@gmail.com>
2020-02-23 21:56:26 +0530

    demo

--- src/main/java/com/poc/flagpicker/FlagpickerApplication.java
+++ src/main/java/com/poc/flagpicker/FlagpickerApplication.java
@@ -3,15 +3,7 @@ package com.poc.flagpicker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Spring boot applicationn
-//Json added
-//Query file added
-
-//checked query file
-//Double checks done
-
-//Second query file
-//new
+//demo
 @SpringBootApplication
 public class FlagpickerApplication {
 
