a1358ee43f6c4b2dc6f6c69dbab4e66c52ce57b1
Rashmi <rashmiagrawal3008@gmail.com>
2020-02-23 13:49:33 +0530

    Second Commit

--- src/main/java/com/poc/flagpicker/FlagpickerApplication.java
+++ src/main/java/com/poc/flagpicker/FlagpickerApplication.java
@@ -3,6 +3,7 @@ package com.poc.flagpicker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Spring boot application
 @SpringBootApplication
 public class FlagpickerApplication {
 
