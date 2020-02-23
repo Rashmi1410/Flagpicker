a0ec19ef9047d04e7b7ff662c31bc4087b580056
Rashmi <rashmiagrawal3008@gmail.com>
2020-02-23 14:45:20 +0530

    Json added

--- src/main/java/com/poc/flagpicker/FlagpickerApplication.java
+++ src/main/java/com/poc/flagpicker/FlagpickerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Spring boot applicationn
+//Json added
 @SpringBootApplication
 public class FlagpickerApplication {
 
